0a1efdb66f13034ebb6f138668b9774418a31173
natemiller1 <36780196+natemiller1@users.noreply.github.com>
2019-01-23 16:25:49 -0500

    Update Stream.py

--- Stream.py
+++ Stream.py
@@ -1,7 +1,14 @@
-import tweepy
+import tweepy, harmony_connect_client
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+#Define Listening Class
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
 
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
