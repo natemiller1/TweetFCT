02504451f25883db8e454e7c47870c623d33130b
natemiller1 <36780196+natemiller1@users.noreply.github.com>
2019-01-29 16:47:40 -0500

    Create private.py

---
+++ private.py
@@ -0,0 +1,8 @@
+#update this with your Twitter API keys, you'll need to sign up for a dev account first: https://developer.twitter.com/
+
+TWITTER_KEY = 
+TWITTER_SECRET = 
+TWITTER_APP_KEY = 
+TWITTER_APP_SECRET = 
+
+#CONNECTION_STRING -- use sqlite:///tweets.db as a default if you need to. It's recommended to use postgresql, but not necessary.
