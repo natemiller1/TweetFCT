9c74f3074d138d25d0c21cb10f7345bf19cff1ed
natemiller1 <36780196+natemiller1@users.noreply.github.com>
2019-02-11 15:36:53 -0500

    Update Stream.py

--- Stream.py
+++ Stream.py
@@ -21,6 +21,7 @@ walletd = FactomWalletd(
     username='rpc_username',
     password='rpc_password'
 )
+
 chain_id = 'da2ffed0ae7b33acc718089edc0f1d001289857cc27a49b6bc4dd22fac971495' #this will need to be automated to create a new chain for each twitter account tracked
 
 auth = tweepy.OAuthHandler(settings.TWITTER_APP_KEY, settings.TWITTER_APP_SECRET)
@@ -28,18 +29,12 @@ auth.set_access_token(settings.TWITTER_KEY, settings.TWITTER_SECRET)
 api = tweepy.API(auth)
 
 #Define Listening Class
-class MyStreamListener(tweepy.StreamListener):
+class StreamListener(tweepy.StreamListener):
 
     def on_status(self, status):  #Tweets will need to be filtered, twitter default pulls ALL tweets with the username you're tracking
-        if '@realDonaldTrump'in status.text:
-            return
-  #      if '@RealDonaldTrump' in status.text:
-   #         return
-        if '@realdonaldtrump' in status.text:
-            return
         if 'factombot' in status.text:
             return
-        if status.in_reply_to_user_id_str is 25073877:  #this filters replies to this twitter ID
+        if status.in_reply_to_user_id_str is 1088451823642595328:  #this filters replies to this twitter ID
             return
         
         name = status.user.screen_name #pulls username of tweeter
@@ -49,8 +44,8 @@ class MyStreamListener(tweepy.StreamListener):
         
         walletd.new_entry(factomd, chain_id, ['random', 'entry', 'id'], fct_entry, ec_address=ec_address) #makes entry into the factom testnet
 
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+StreamListener = MyStreamListener()
+stream = tweepy.Stream(auth = api.auth, listener=StreamListener)
 
-myStream.filter(follow=["2211149702"]) #change number to whatever twitter ID (or IDs) you want to follow (not the @ handle), you can look up username ID's here: https://tweeterid.com/
-stream.filter(track=settings.TRACK_TERMS)
+stream.filter(follow=["1088451823642595328"]) #change number to whatever twitter ID (or IDs) you want to follow (not the @ handle), you can look up username ID's here: https://tweeterid.com/
+#stream.filter(track=settings.TRACK_TERMS)  #can also track via settings.py
